cd68d835b6bc1c99c1c9b78739794ad5095773dc
Resham <resha@Mac.attlocal.net>
2025-09-13 21:18:53 -0700

    Added Code Comments

--- main.cpp
+++ main.cpp
@@ -2,18 +2,26 @@
 #include <ctime>
 using namespace std;
 
+// Comment #1: Define a global constant for the array size of 5 elements.
 const int SIZE = 5; 
 
+// Comment #2: Function prototypes for entering data, outputting data, and summing the array elements.
 void enterArrayData(double *ptr);
 void outputArrayData(double *ptr);
 double sumArray(double *ptr);
 
 int main()
 {
+    // Comment #3: Dynamically allocate an array of doubles and use the defined functions.
     double *arr = new double[SIZE];
+
+    // Comment #4: Prompt the user to enter data into the array.
     enterArrayData(arr);
+
+    // Comment #5: Output the array elements separated by spaces.
     outputArrayData(arr);
     
+    // Comment #6: Calculate and display the sum of the array elements.
     cout << "Sum of values: " << sumArray(arr) << endl;
 
     delete [] arr;
